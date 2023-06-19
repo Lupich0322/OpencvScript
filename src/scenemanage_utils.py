ea3414d2813624bb56dcded459eda0a93c46d5d5
Lupich0322 <huajibupi@gmail.com>
2023-06-19 18:03:54 +0800

    Improve scene navigation

--- src/scenemanage_utils.py
+++ src/scenemanage_utils.py
@@ -7,6 +7,8 @@ from queue import PriorityQueue
 # 加载场景转换信息
 with open('scene_transitions.json', 'r') as file:
     transitions = json.load(file)
+# 生成邻接表
+adjacency_list = {scene: list(transitions.keys()) for scene, transitions in transitions.items()}
 
 def load_scene_transitions(json_path):
     with open(json_path, 'r') as f:
@@ -14,29 +16,20 @@ def load_scene_transitions(json_path):
     return transitions
 
 def get_current_scene(device, scene_templates_dir):
-    # 获取当前屏幕截图
     screenshot_image = get_screenshot(device)
     # 遍历所有场景模板
     for scene in os.listdir(scene_templates_dir):
         scene_dir = os.path.join(scene_templates_dir, scene)
         if os.path.isdir(scene_dir):
-            # 读取场景文件夹下的图片
             for file_name in os.listdir(scene_dir):
                 if file_name.endswith(('.jpg', '.jpeg', '.png')):
                     template_path = os.path.join(scene_dir, file_name)
-                    # 加载模板
                     template = load_template(template_path)
-                    # 执行模板匹配
                     result = cv2.matchTemplate(screenshot_image, template, cv2.TM_CCOEFF_NORMED)
                     _, max_val, _, max_loc = cv2.minMaxLoc(result)
-                    # 如果匹配度大于阈值，例如0.8，则认为当前场景是这个场景
                     if max_val > 0.8:
                         return scene
-    # 如果没有匹配到任何场景，返回None
     return None
-
-# 生成邻接表
-adjacency_list = {scene: list(transitions.keys()) for scene, transitions in transitions.items()}
 
 # A* 搜索算法
 def a_star_search(start, end):
@@ -92,25 +85,21 @@ def navigate_to_scene(device, start_scene, end_scene):
     for i in range(len(path) - 1):
         current_scene = path[i]
         next_scene = path[i + 1]
-        transition = transitions[current_scene][next_scene]
+        transitions_sequence = transitions[current_scene][next_scene]  # 获取转换步骤列表
 
-        while True:
-            # 获取屏幕截图
-            screenshot_image = get_screenshot(device)
-            # 加载场景转换模板
-            template = load_template(os.path.join("../images/scene_templates", current_scene, transition))
-            # 模板匹配
-            result = cv2.matchTemplate(screenshot_image, template, cv2.TM_CCOEFF_NORMED)
-            _, max_val, _, max_loc = cv2.minMaxLoc(result)
+        for transition in transitions_sequence:
+            while True:
+                screenshot_image = get_screenshot(device)
+                template = load_template(os.path.join("../images/scene_templates", current_scene, transition))
+                result = cv2.matchTemplate(screenshot_image, template, cv2.TM_CCOEFF_NORMED)
+                _, max_val, _, max_loc = cv2.minMaxLoc(result)
 
-            if max_val > 0.8:
-                # 计算点击位置
-                center_x = max_loc[0] + template.shape[1] // 2
-                center_y = max_loc[1] + template.shape[0] // 2
-                # 点击
-                click_position(device, center_x, center_y)
-                time.sleep(1)  # 等待场景转换
+                if max_val > 0.8:
+                    center_x = max_loc[0] + template.shape[1] // 2
+                    center_y = max_loc[1] + template.shape[0] // 2
+                    click_position(device, center_x, center_y)
+                    time.sleep(0.5)  # 等待场景转换
 
-                if get_current_scene(device, "../images/scene_markers") == next_scene:
-                    break  # 如果已经到达下一个场景，则跳出循环
-
+                    if get_current_scene(device, "../images/scene_markers") == next_scene:
+                        print(get_current_scene(device, "../images/scene_markers"))
+                        break  # 如果已经到达下一个场景，则跳出循环
